b344ddbdea26c5c7e2748881a95a275237b326d9
MrFutureBoss <maitu10e2@gmail.com>
2024-10-07 02:51:03 +0700

    update profession management

--- src/pages/ProfessionManagement.jsx
+++ src/pages/ProfessionManagement.jsx
@@ -3,36 +3,100 @@ import { BASE_URL } from "../utilities/initalValue.js";
 import { useDispatch, useSelector } from "react-redux";
 import { setProfessions } from "../redux/slice/ProfessionSlice.js";
 import { useEffect } from "react";
+import { Card, Button, Col, Container, Row, Pagination } from "react-bootstrap";
+import {
+  EditOutlined,
+  LockOutlined,
+  PlusCircleOutlined,
+  UnlockOutlined,
+} from "@ant-design/icons";
 
 const ProfessionManagement = () => {
   const dispatch = useDispatch();
-  const professions = useSelector((state) => state.profession.professions.data); // Access the data array
-  const total = useSelector((state) => state.profession.professions.total); // Access the total count
+  const professions = useSelector((state) => state.profession.professions.data);
+  const total = useSelector((state) => state.profession.professions.total);
 
   // Call profession data
   useEffect(() => {
     axios
       .get(`${BASE_URL}/profession`)
       .then((res) => {
+        console.log("Total: " + res.data.total);
         dispatch(setProfessions(res.data)); // Pass the entire response to the reducer
       })
       .catch((err) => console.log("Error fetching professions", err));
   }, [dispatch]);
-  console.log("Total: "+ total)
 
+
+  const onChange = (pageNumber) => {
+    console.log('Page: ', pageNumber);
+  };
+  
   return (
-    <>
-      <h1>Profession Management</h1>
-      <p>Total Professions: {total}</p> {/* Display total count */}
-      <div>
+    <Container fluid>
+      <Row>
+        <h1>Profession & Specialty Management</h1>
+      </Row>
+      <Row style={{ marginBottom: "10px" }}>
+        <Button style={{ width: "10rem" }} variant="primary">
+          <PlusCircleOutlined /> Thêm lĩnh vực
+        </Button>
+      </Row>
+      <Row>
         {professions.map((pro) => (
-          <div key={pro._id}>
-            <p>ID: {pro._id}</p>
-            <p>Name: {pro.name}</p>
-          </div>
+          <Col sm={6} key={pro._id} style={{ margin: "auto" }}>
+            <Card style={{ width: "100%", marginBottom: "5px" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Row>
+                  <Col sm={9}>
+                    <Card.Title>{pro.name}</Card.Title>
+                  </Col>
+                  <Col
+                    sm={3}
+                    style={{
+                      margin: "auto",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "end",
+                      gap: "4px",
+                    }}
+                  >
+                    {pro.status ? (
+                      <LockOutlined
+                        style={{
+                          color: "red",
+                          fontSize: "34px",
+                        }}
+                      />
+                    ) : (
+                      <UnlockOutlined
+                        style={{
+                          color: "green",
+                          fontSize: "34px",
+                        }}
+                      />
+                    )}
+
+                    <Button
+                      style={{ borderWidth: "2px" }}
+                      Button
+                      variant="outline-primary"
+                    >
+                      <EditOutlined twoToneColor="#FFF" />
+                    </Button>
+                  </Col>
+                </Row>
+                <Card.Text>Chuyên môn:</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </div>
-    </>
+      </Row>
+      <Row>
+      <Pagination showQuickJumper defaultCurrent={2} total={500} onChange={onChange} />
+      </Row>
+    </Container>
   );
 };
 
