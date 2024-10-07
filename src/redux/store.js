aa33b1b93e1a58c3ccd183e82b1d78146bdc7f99
tuanfec <132864211+tuanfec@users.noreply.github.com>
2024-10-07 22:28:09 +0700

    semesterSlide

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware } from "redux";
 import professionSlice from "./slice/ProfessionSlice.js";
 import * as thunk from "redux-thunk";
 import { configureStore } from "@reduxjs/toolkit";
+import semesterSlide from "./slice/semesterSlide.js";
 
 const rootReducer = {
   profession: professionSlice,
+  semester: semesterSlide,
 };
 
 const store = configureStore(
