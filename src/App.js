fb30d6fde60ba4f95266b70f44da5a121ff48023
Anhtu227 <tudahe160774@fpt.edu.vn>
2024-10-14 20:53:20 +0700

    ChangePassword.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ChangePassword from './ChangePassword';
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ProfessionManagement from "./pages/ProfessionManagement.jsx";
+import ChangePassword from './ChangePassword'; 
+
+//Route tạm thời để code không dùng thì comment lại
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Change Password Page</h1>
-        <ChangePassword />
+        <h1>Change Password Example</h1>
+        <p>Click the link below to change your password:</p>
+
+       <ChangePassword />
+        <Routes> 
+        <Route path="/" element={<ProfessionManagement />} />
+      </Routes>
       </header>
     </div>
+      
+      
   );
 }
 
