2e2ae65985f6f6501629a144b0da3edcc60a9a0d
MrFutureBoss <maitu10e2@gmail.com>
2025-02-06 23:32:31 +0700

    ok

--- client/src/utilities/initalValue.js
+++ client/src/utilities/initalValue.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:9999";
+// const BASE_URL = "http://localhost:9999";
+const BASE_URL = "https://demo-edustart-be.onrender.com";
 const jwt = localStorage.getItem("jwt");
 
 const config = {
